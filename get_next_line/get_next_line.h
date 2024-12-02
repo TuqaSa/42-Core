e87b02b8c8a718dd4510ec0b9c62ca2abdeb2378
TuqaSa <saeedtuqa@gmail.com>
2024-12-02 22:44:07 +0300

    done

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsaeed <tsaeed@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 22:20:23 by tsaeed            #+#    #+#             */
+/*   Updated: 2024/12/01 02:05:34 by tsaeed           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+char	*ft_read_file(int fd, char *keep);
+char	*ft_extract_line(char *keep);
+char	*ft_delete_read(char *keep);
+
+#endif
